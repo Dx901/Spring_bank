0954bdafd22f951eafa98fffcd6442c3e03edf86
Dyes Junior Sakwa <donjnr95@gmail.com>
2023-12-22 13:10:27 +0300

    Test to find all users passed

--- src/test/java/com/dyes/bank/repository/UserRepositoryTest.java
+++ src/test/java/com/dyes/bank/repository/UserRepositoryTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 public class UserRepositoryTest {
 
@@ -27,5 +29,22 @@ public class UserRepositoryTest {
         assertNotNull(savedUser.getUserId());
         assertEquals("John Doe", savedUser.getName());
 
+    }
+
+    @Test
+    public void getUsers_ShouldReturnAllUsers() {
+        User user1 = new User();
+        user1.setName("John Pombe");
+
+        User user2 = new User();
+        user2.setName("JAne Doe");
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        List<User> allUsers = userRepository.findAll();
+
+        //then
+        assertEquals(2, allUsers.size());
     }
 }
